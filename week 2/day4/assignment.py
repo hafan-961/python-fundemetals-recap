ace7fb7b81891ebb771ca95c2bda35812d9924a4
muhammed hafan <muhammedhafan961@gmail.com>
2026-01-30 00:12:14 +0530

    adding first assignment

--- week 2/day4/assignment.py
+++ week 2/day4/assignment.py
@@ -1,8 +1,68 @@
 '''
-Design a console ased library management system a 
+Design a console based library management system a 
  a libraryitem is the base(abstract) idea
  book and magazine aree differenct type of items 
  borrowing rules behave diff for each of them
  a liabraryApp conroles items (HAS-A)
  use oops concept and return value , not only print
  '''
+class Library_Item:           #Abstract class
+    def __init__(self, student_id, name, member, days):
+        self.student_id = student_id
+        self._name = name        # encapsulated
+        self._member = member     # encapsulated
+        self._days = days         # protected
+
+    def is_member(self):
+        return self._member
+
+    def calculate_rent(self):
+        pass   # only rule 
+
+#Interface class
+class interface:
+    def calculate_rent(self):
+        pass
+
+class Book(Library_Item):   #Inhertitance
+    def __init__(self, student_id, name, member, days):
+        super().__init__(student_id, name, member, days)  
+
+    def calculate_rent(self):     # polymorphism
+        if self._member == "yes":
+            return 0
+        else:
+            return self._days * 10
+
+
+class Magazine(Library_Item):  #Inheritance
+    def __init__(self, student_id, name, member, days):
+        super().__init__(student_id, name, member, days)
+
+    def calculate_rent(self):    #polymorphism
+        if self._member == "yes":
+            return 0
+        else:
+            return self._days * 20
+
+#composition class
+class LibraryApp:
+    def __init__(self):
+        self.item = None
+
+    def item_taken(self, item_type, student_id, name, member, days):
+        if item_type == "Book":
+            self.item = Book(student_id, name, member, days)
+        elif item_type == "Magazine":
+            self.item = Magazine(student_id, name, member, days)
+
+        print("Item issued")
+
+    def get_rent(self):
+        return self.item.calculate_rent()
+
+
+
+app = LibraryApp()
+app.item_taken("Magazine", 1, "hafan", "no", 55)
+print(app.get_rent())
--- week 2/day4/project2.py
+++ week 2/day4/project2.py
@@ -30,14 +30,14 @@ class full_time_employee(Employee):
     def calculate_salary(self):
         return self._monthly_salary
 
-class part_time_empoyee(Employee):
+class part_time_employee(Employee):
     def __init__(self,emp_id,name,hours_worked,rate_per_hour):
         super().__init__(emp_id,name)
         self._hours_worked = hours_worked
         self._rate_per_hour = rate_per_hour
     
     def calculate_salary(self):
-        return self._hourrs_worked * self._rate_per_hour
+        return self._hours_worked * self._rate_per_hour
 
 class PayrollApp:
     def __init__(self):
@@ -45,20 +45,23 @@ class PayrollApp:
     
     def create_employee(self,emp_type):
         if emp_type == "FullTime":
-            return full_time_employee(1,"Amit" , 50000)
+            self.employee = full_time_employee(1,"Amit" , 50000)
         else:
-            return part_time_employee(2,"Ruban" , 5, 400)
+            self.employee = part_time_employee(2,"Ruban" , 5, 400)
+        print("Employee created")
     
     def get_salary(self):
         return self.employee.calculate_salary()
 
-app = PayrollApp()
-print(app.create_employee("FullTime"))
-print
-app2 = PayrollApp()
-print(app.create_employee("PartTime"))
-print()
 
+
+
+app = PayrollApp()
+app.create_employee("FullTime")
+print('salary: ',app.get_salary())
+app2 = PayrollApp()
+app2.create_employee("partime")
+print('salary: ',app.get_salary())
 
 
 
